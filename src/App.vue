a0384ad96f732725d065648a300d5c7a27d0bcc0
Jolangalifk <jolangalifk@gmail.com>
2023-11-23 17:01:38 +0700

    add: [JK] navbar

--- src/App.vue
+++ src/App.vue
@@ -1,102 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <h1>Ini cuma ngetes</h1>
-    <div>
-      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="https://picsum.photos/seed/picsum/200/300" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="https://picsum.photos/seed/picsum/200/300" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="https://picsum.photos/seed/picsum/200/300" class="d-block w-100" alt="...">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div>
-  </div>
+  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-black px-5 fixed-top">
+        <div class="container-fluid d-flex justify-content-between mx-5 px-5">
+            <div class="d-flex align-items-center">
+                <a class="navbar-brand" href="#">
+                    <img src="@/assets/image/aset-logo-creativelabz.png" width="50" height="50" alt="">
+                </a>
+            </div>
+            <div class="collapse navbar-collapse d-flex justify-content-end " id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"
+                            style="color: #D6D6D6; font-weight: bold;">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: #D6D6D6; font-weight: bold;">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: #D6D6D6; font-weight: bold;">Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: #D6D6D6; font-weight: bold;">News & Event</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: #D6D6D6; font-weight: bold;">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" style="color: #D6D6D6; font-weight: bold;">My account</a>
+                    </li>
+                    <li class="nav-item d-flex align-items-center justify-content-center">
+                        <img src="@/assets/icon/cart.svg" alt="">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+  
