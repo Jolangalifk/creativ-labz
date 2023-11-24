cc0ea1879944cbd61899a6a247dc9c6f7e8e938c
rezasetyawan <setyawanreza960@gmail.com>
2023-11-24 14:14:02 +0700

    add: footer and about us

---
+++ src/components/Footer.vue
@@ -0,0 +1,72 @@
+<template>
+    <footer id="footer">
+        <div class="row pt-5 justify-content-between mx-1 mx-lg-5 px-lg-5">
+            <div class="contact text-center col-12 col-lg-4">
+                <h4>Visit Us</h4>
+                <p>Sunday – Monday : 08.00 am – 10.00 pm</p>
+            </div>
+            <div class="contact text-center col-12 col-lg-4">
+                <h4>Call Us</h4>
+                <p>085254018888</p>
+            </div>
+            <div class="contact text-center col-12 col-lg-4">
+                <h4>Address</h4>
+                <p>Jl. Mangga Besar IV R No.44, RT.2/RW.1, Taman Sari, Jakarta Barat</p>
+            </div>
+        </div>
+        <div class="socials-media d-flex gap-4 align-items-center justify-content-center mt-5">
+            <a href="#" class="d-flex align-items-center justify-content-center"><img src="@/assets/icons/fb.png"></a>
+            <a href="#" class="d-flex align-items-center justify-content-center"><img src="@/assets/icons/x.png"></a>
+            <a href="https://www.instagram.com/creativlabz/" rel="norefferer noopener"
+                class="d-flex align-items-center justify-content-center"><img src="@/assets/icons/ig.png"></a>
+        </div>
+    </footer>
+</template>
+<style scoped>
+#footer {
+    font-weight: 500;
+    width: 100%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: linear-gradient(180deg, #000000 21%, rgba(10, 10, 10, 0.58) 100%),
+        url('src/assets/images/footer-bg.webp') !important;
+    background-position: center;
+    padding-bottom: 8rem;
+}
+
+.contact>h4 {
+    font-family: 'Quattrocento', 'Times New Roman', Times, serif;
+    font-size: 1rem;
+    line-height: 130%;
+    color: #00c0ab;
+}
+
+.contact>p {
+    font-family: 'Quattrocento', 'Times New Roman', Times, serif;
+    font-size: 0.875rem;
+    line-height: 177%;
+    color: white;
+}
+
+.socials-media>a {
+    background-color: rgba(0, 0, 0, 0.3);
+    padding: 1rem;
+    border-radius: 5px;
+    width: 2.8rem;
+    height: 2.8rem;
+}
+
+.socials-media>a>img {
+    height: 1.125rem;
+}
+
+@media (min-width: 1024px) {
+    .contact>h4 {
+        font-size: 1.875rem;
+    }
+
+    .contact>p {
+        font-size: 1.125rem;
+    }
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,192 @@
+<script setup lang="ts">
+import Navbar from "../components/Navbar.vue"
+import Footer from "../components/Footer.vue"
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+  <Navbar />
+  <section id="about-us">
+    <div class="about-info">
+      <div class="about">
+        <h2>About Us</h2>
+        <h1>Creativ Labz</h1>
+        <p>
+          PT Kreatif Vapelab Indonesia merupakan salah satu distributor terbesar Liquid USA . kami berpengalaman dari
+          tahun
+          2016 hingga sekarang.
 
-<style>
+          Sebagai perusahaan distributor berpengalaman dari 2016, kami memberikan harga yang sangat bersaing dengan
+          kualitas
+          terbaik.
+
+          Contact Us
+        </p>
+        <button class="btn rounded-5 px-3">Contact Us</button>
+      </div>
+    </div>
+    <div class="strenght mb-5">
+      <h2 class="text-center mt-4 mb-5">Why Choose Us?</h2>
+      <div class="row justify-content-center gap-4 gap-sm-0 p-3">
+        <div class="col-12 col-md-4 strenght-item d-flex flex-column align-items-center justify-content-center">
+          <img src="@/assets/images/about-us/truck-fast-solid.svg" width="96  " height="96">
+          <h3 class="mt-2">Pengiriman Cepat</h3>
+          <p class="text-center">Melayani pengiriman produk yang cepat</p>
+        </div>
+        <div class="col-12  col-md-4 strenght-item d-flex flex-column align-items-center justify-content-center">
+          <img src="@/assets/images/about-us/wallet.svg" width="96" height="96">
+          <h3 class="mt-2">Harga Terbaik</h3>
+          <p class="text-center">Penjualan Prduk dengan harga terbaik</p>
+        </div>
+        <div class="col-12  col-md-4 strenght-item d-flex flex-column align-items-center justify-content-center">
+          <img src="@/assets/images/about-us/medal.svg" width="96" height="96">
+          <h3 class="mt-2">Produck Berkualitas</h3>
+          <p class="text-center">Jaminan produk berkualitas</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <Footer />
+</template>
+<style scoped>
+#about-us {
+  font-family: 'Poppins', sans-serif;
+  overflow-x: hidden;
+}
+
+.about-info {
+  width: 100%;
+  background-image: url('src/assets/images/about-us/baackground-cl-e1679853251444.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  margin-top: 4rem;
+  padding: 2rem 1rem;
+}
+
+.about-info>.about {
+  background-color: white;
+  padding: 2rem 1rem;
+  border-radius: 20px;
+  width: 100%;
+}
+
+.about>h2 {
+  color: #00c0ab;
+  font-size: 0.9rem;
+  line-height: 140%;
+  font-weight: 600;
+}
+
+.about>h1 {
+  font-size: 1.125rem;
+  font-weight: 700;
+  line-height: 150%;
+  color: #666666;
+}
+
+.about>p {
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 118%;
+  color: #666666;
+}
+
+.about>button {
+  background-color: #00c0ab;
+  color: white;
+  font-size: 0.8rem;
+  line-height: 170%;
+
+}
+
+.strenght {
+  background-color: white;
+}
+
+.strenght>h2 {
+  font-size: 1.125rem;
+  line-height: 140%;
+  font-weight: 700;
+  color: #666666;
+}
+
+.strenght-item>h3 {
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 118%;
+  color: #333333;
+  text-align: center;
+}
+
+.strenght-item>p {
+  font-size: 0.875rem;
+  font-weight: 500;
+  color: #666666;
+  line-height: 170%;
+}
+
+@media (min-width: 768px) {
+  .about-info {
+    padding: 3rem;
+  }
+
+  .about-info>.about {
+    padding: 2.5rem 1.5rem;
+    border-radius: 20px;
+    width: 75%;
+    margin: 0 auto;
+  }
+}
+
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+  .about-info {
+    padding-top: 1rem;
+    padding-bottom: 4rem;
+  }
+
+  .about-info>.about {
+    padding: 3rem 1.5rem;
+    width: 50%;
+    margin: 0;
+    margin: 3% 0 0 3%;
+  }
+}
+
+@media (min-width: 1200px) {
+  .about-info {
+    padding-bottom: 6rem;
+  }
+
+  .about-info>.about {
+    padding: 3rem 1.5rem;
+    width: 40%;
+    margin: 4% 0 0 8%;
+  }
+
+  .about>h2 {
+    font-size: 1.25rem;
+  }
+
+  .about>h1 {
+    font-size: 4rem;
+  }
+
+  .about>p {
+    font-size: 1.125rem;
+  }
+
+  .about>button {
+    font-size: 1rem;
+  }
+
+  .strenght {
+    background-color: white;
+  }
+
+  .strenght>h2 {
+    font-size: 2.25rem;
+  }
+
+  .strenght-item>h3 {
+    font-size: 1.125rem;
   }
 }
 </style>
